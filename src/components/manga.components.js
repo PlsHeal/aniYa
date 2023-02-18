4fbc2f115aac1a7ec62da76781bf50d5a532fb57
PlzHeal <stechnology357@gmail.com>
2023-02-18 12:20:34 +0800

    add pagination section

--- src/components/manga.components.js
+++ src/components/manga.components.js
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import "../styles/pagination.component.css"
+
+function Pagination() {
+    return (
+        <nav aria-label="Page navigation example" style={{display: "grid", placeItems: "center"}}>
+            <ul className="pagination">
+                <li className="page-item"><Link className="page-link" href="#">        
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                    </Link>
+                </li>
+                <li className="page-item"><Link className="page-link" href="#">1</Link></li>
+                <li className="page-item"><Link className="page-link" href="#">2</Link></li>
+                <li className="page-item"><Link className="page-link" href="#">3</Link></li>
+                <li className="page-item"><Link className="page-link" href="#">        
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
 export default function MangaSection() {
     return(
-        <p>This is manga section</p>
+        <Pagination />
     );
 }
